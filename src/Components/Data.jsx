5e5f4faca7cb5f170b0cabcf34af6c7873c8c07e
Michael Wood <michaelwood33311@icloud.com>
2023-03-25 12:42:55 -0400

    added dropzone

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -1,39 +1,46 @@
 import './Data.scss';
 import React from 'react'
-
+import Dropzone from 'react-dropzone';
 
 function Data({section, updateOption, updateSection}) {
-    const option = section.option;
-    const chart = option && option.info && option.info.chart ? option.info.chart : '';
-
+    
     const capitalized = word => word.charAt(0).toUpperCase() + word.slice(1);
 
   return (
     <div className='Data'>
-         {!chart && <h2 className="file-upload--input">Select Chart Type</h2>}
-            {chart && <h2 className="file-upload--input">{capitalized(chart)} Chart</h2> }
-            {/* <div className='file-upload--fileName'>{fileName}</div> */}
-            {!chart && <select id="chartType" name = "chartType" className='file-upload--select' onChange={e => updateSection({curChart: e.target.value})}>
-                <option value=''>---</option>
-                <option value="bar">&nbsp;Bar</option>
-                <option value="line">&nbsp;Line</option>
-                <option value="pie">&nbsp;Pie</option>
-                <option value="stack">&nbsp;Stack</option>
-            </select> 
-            }
-           {/* {chart && !csv.length && <div className="dropzone-container">
-            <Dropzone 
-                onDrop={acceptedFiles => uploadFiles(acceptedFiles)}>
-                    {({getRootProps, getInputProps}) => (
-                        <section>
-                        <div {...getRootProps()}>
-                            <input {...getInputProps()} />
-                            <p>Drag 'n' drop chart csv file here.<br /> Or click to select file</p>
-                        </div>
-                        </section>
-                    )}
-                </Dropzone>
-           </div>} */}
+        <h1 className='Data__heading'>Data</h1>
+         <div className="Data__choices">
+            <div className="Data__chart-type">
+                {!section.curChart && <h2 className="Data__instructions">Select Chart Type</h2>}
+                {section.curChart && <h2 className="Data__">{capitalized(section.curChart)} Chart</h2> }
+                    {/* <div className='file-upload--fileName'>{fileName}</div> */}
+                    {<select id="chartType" name = "chartType" className='file-upload--select' onChange={e => updateSection({curChart: e.target.value})} value={section.curChart ? section.curChart : ''}>
+                        <option value=''>---</option>
+                        <option value="bar">&nbsp;Bar</option>
+                        <option value="line">&nbsp;Line</option>
+                        <option value="pie">&nbsp;Pie</option>
+                        <option value="stack">&nbsp;Stack</option>
+                    </select> 
+                    }
+                    {<div className={section.curChart ? "dropzone-container" : "dropzone-container dropzone-container--hidden"}>
+                        <Dropzone 
+                            //onDrop={acceptedFiles => uploadFiles(acceptedFiles)}
+                        >
+                                {({getRootProps, getInputProps}) => (
+                                    <section>
+                                    <div {...getRootProps()}>
+                                        <input {...getInputProps()} />
+                                        <p>Drag 'n' drop chart csv file here.<br /> Or click to select file</p>
+                                    </div>
+                                    </section>
+                                )}
+                        </Dropzone>
+                    </div>}
+                </div>
+                <h2 className="Data__csv">CSV</h2>   
+               
+         </div>
+        
            {/* {!embedCode && <div ref={metaAreaRef} className="file-upload__chartMetaContainer">
                 <h3 className='file-upload__metaDataLabel'>Meta Data</h3>
                 <textarea 
