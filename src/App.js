baf70055296548ac06672b0ab1c4af2bf7dba077
Michael Wood <michaelwood33311@icloud.com>
2023-03-25 11:49:40 -0400

    created universal updateOption function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import logo from './logo.svg';
 import './App.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Data from './Components/Data';
+import { merge, cloneDeep } from 'lodash';
 
 function App() {
   const selections = [
     'Data'
   ]
   let template = {
-
+    
   };
 
   const [selection, setSelection] = useState('Data');
   const [option, setOption] = useState(template);
+
+  const updateOption = data => {
+    console.log('data', data);
+    const optionCopy = cloneDeep(option);
+    merge(optionCopy, data);
+    console.log('optionCopy', optionCopy)
+    setOption(optionCopy);
+  }
+
+  console.log('option', option);
+  option && option.info && option.info.changes();
+
+  useEffect(() => {
+    updateOption({
+      info: {
+        chart: 'bar',
+        changes: () => console.log('stuff')
+      }
+    })
+  
+    
+  }, [])
 
   return (
     <div className="App">
