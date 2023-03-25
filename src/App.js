245cc90a761679829e2845f730b7373f27e47fcf
Michael Wood <michaelwood33311@icloud.com>
2023-03-25 11:39:32 -0400

    added framework for Data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState } from 'react';
+import Data from './Components/Data';
 
 function App() {
+  const selections = [
+    'Data'
+  ]
+  let template = {
+
+  };
+
+  const [selection, setSelection] = useState('Data');
+  const [option, setOption] = useState(template);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {selection === 'Data' && <Data />}
     </div>
   );
 }
---
+++ src/Components/Data.jsx
@@ -0,0 +1,13 @@
+import './Data.scss';
+import React from 'react'
+
+
+function Data() {
+  return (
+    <div className='Data'>
+        Data
+    </div>
+  )
+}
+
+export default Data
