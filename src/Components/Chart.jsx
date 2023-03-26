0296054e629cb0197c4556cfe350d6c72dd88028
Michael Wood <michaelwood33311@icloud.com>
2023-03-26 13:06:31 -0400

    added function to calculate dimensions of chart components

--- src/Components/Chart.jsx
+++ src/Components/Chart.jsx
@@ -4,13 +4,32 @@ import React, { useEffect, useRef } from 'react';
 function Chart({option}) {
   const chartRef = useRef();
 
-  
+  function getSeriesInfo(chart) {
+    console.log(chart.getModel().getSeries());
+  }
 
-  useEffect(() => {
+  function getComponentDimensions(chart, name, index = -1) {
+      const model = chart.getModel().getComponent(name);
+      const view = chart.getViewOfComponentModel(model);
+      const rect = view.group.getBoundingRect();
+      const [x, y] = view.group.transformCoordToGlobal(rect.x, rect.y)
+      const { width, height } = rect;
+      const dimensions = {x, y, width, height};
+      return dimensions;
+  }
+
+  const displayChart = () => {
     const chartDom = chartRef.current;
     const echarts = window.echarts;
     var myChart = echarts.init(chartDom);
     myChart.setOption(option);
+    console.log('container dimensions', chartDom.clientWidth, chartDom.clientHeight);
+    if (option.title.length) console.log('title dimensions', getComponentDimensions(myChart, 'title'));
+
+  }
+
+  useEffect(() => {
+    displayChart();
   })
 
   return (
