e8a9891bf8f2738753e469385fec7cfdfbdb2a21
Michael Wood <michaelwood33311@icloud.com>
2023-03-26 10:52:31 -0400

    completed basic pie chart integration

--- src/Components/Chart.jsx
+++ src/Components/Chart.jsx
@@ -5,8 +5,10 @@ function Chart({option}) {
   const chartRef = useRef();
 
   const chartDom = chartRef.current;
+  const echarts = window.echarts;
+  var myChart = echarts.init(chartDom);
+  myChart.setOption(option);
 
-  
   return (
     <div className='chart'>
         <h1 className="chart__heading">Chart</h1>
